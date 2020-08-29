9b405d6c15752df0e0f7abe0baefe6629c6de724
geqiang <geqiang@xin.com>
2020-08-29 16:01:55 +0800

    fixed

---
+++ src/links.json.js
@@ -0,0 +1,440 @@
+var links = [
+    {
+        "c": "编程开发",
+        "a": [
+            {
+                "name": "Kernel",
+                "url": "https://www.kernel.org/"
+            },
+            {
+                "name": "Go官网",
+                "url": "https://golang.google.cn/"
+            },
+            {
+                "name": "GoProxy",
+                "url": "https://goproxy.cn"
+            },
+            {
+                "name": "Go代理",
+                "url": "https://goproxy.io/zh/"
+            },
+            {
+                "name": "PHP在线运行",
+                "url": "https://tool.lu/coderunner"
+            },
+            {
+                "name": "代码在线运行",
+                "url": "http://www.dooccn.com/go/"
+            },
+            {
+                "name": "代码格式化",
+                "url": "https://tool.oschina.net/codeformat/json"
+            },
+            {
+                "name": "PostWoman",
+                "url": "https://postwoman.io/"
+            },
+            {
+                "name": "时间戳转换",
+                "url": "http://www.kjson.com/time/"
+            },
+            {
+                "name": "Json转对象",
+                "url": "https://tool.lu/json/"
+            },
+            {
+                "name": "sql转struct",
+                "url": "http://stming.cn/tool/sql2go.html"
+            },
+            {
+                "name": "加密/解密",
+                "url": "https://tool.oschina.net/encrypt"
+            },
+            {
+                "name": "Rust官网",
+                "url": "https://www.rust-lang.org/"
+            },
+            {
+                "name": "正则表达式",
+                "url": "https://regex101.com/"
+            },
+            {
+                "name": "csv数据转换",
+                "url": "https://tableconvert.com"
+            },
+            {
+                "name": "github镜像",
+                "url": "https://hub.fastgit.org/"
+            },
+            {
+                "name": "开源项目导航",
+                "url": "https://github.com/gokins/open-source-project-navigation"
+            },
+            {
+                "name": "GraphQL",
+                "url": "https://graphql.cn/"
+            },
+            {
+                "name": "生成nginx配置",
+                "url": "https://www.digitalocean.com/community/tools/nginx"
+            },
+            {
+                "name": "生成.gitignore",
+                "url": "https://www.toptal.com/developers/gitignore"
+            },
+            {
+                "name": "mirrors.ustc",
+                "url": "http://mirrors.ustc.edu.cn/"
+            },
+            {
+                "name": "俄罗斯方块",
+                "url": "https://chvin.github.io/react-tetris/?lan=zh"
+            },
+        ]
+    },
+    {
+        "c": "项目管理",
+        "a": [
+            {
+                "name": "Tower",
+                "url": "https://tower.im/"
+            },
+            {
+                "name": "Worktile",
+                "url": "https://worktile.com/"
+            },
+            {
+                "name": "ONES",
+                "url": "https://ones.ai/"
+            },
+            {
+                "name": "teambition",
+                "url": "https://www.teambition.com/"
+            },
+            {
+                "name": "Jira",
+                "url": "https://www.atlassian.com/software/jira"
+            },
+            {
+                "name": "Coding",
+                "url": "https://coding.net/"
+            },
+            {
+                "name": "trello",
+                "url": "https://trello.com/"
+            },
+            {
+                "name": "墨刀",
+                "url": "https://modao.cc/"
+            },
+            {
+                "name": "石墨文档",
+                "url": "https://shimo.im/"
+            },
+            {
+                "name": "腾讯文档",
+                "url": "https://docs.qq.com/desktop"
+            },
+        ]
+    },
+    {
+        "c": "文件传输",
+        "a": [
+            {
+                "name": "奶牛快传",
+                "url": "https://cowtransfer.com"
+            },
+            {
+                "name": "文字快传",
+                "url": "https://cp.anyknew.com/"
+            },
+            {
+                "name": "Fixfox Send",
+                "url": "https://send.firefox.com"
+            },
+            {
+                "name": "文件转换器",
+                "url": "https://convertio.co/zh/"
+            },
+            {
+                "name": "文件格式转换",
+                "url": "https://www.alltoall.net/"
+            },
+            {
+                "name": "文件对比",
+                "url": "https://www.diffchecker.com"
+            },
+            {
+                "name": "文档转换",
+                "url": "https://cn.office-converter.com/"
+            },
+            {
+                "name": "PDF编辑",
+                "url": "https://tools.pdf24.org/zh/"
+            },
+        ]
+    },
+    {
+        "c": "技术社区",
+        "a": [
+            {
+                "name": "阿里云开发者",
+                "url": "https://developer.aliyun.com/"
+            },
+            {
+                "name": "腾讯云社区",
+                "url": "https://cloud.tencent.com/developer"
+            },
+            {
+                "name": "美团技术博客",
+                "url": "https://tech.meituan.com/"
+            },
+            {
+                "name": "阿里中间件",
+                "url": "http://jm.taobao.org"
+            },
+            {
+                "name": "GoPHPer",
+                "url": "https://gocn.vip/"
+            },
+            {
+                "name": "Go技术论坛",
+                "url": "https://learnku.com/go"
+            },
+            {
+                "name": "segmentfault",
+                "url": "https://segmentfault.com"
+            },
+            {
+                "name": "掘金",
+                "url": "https://juejin.im"
+            },
+            {
+                "name": "微信文章",
+                "url": "https://weixin.sogou.com/"
+            },
+            {
+                "name": "Leetcode",
+                "url": "https://leetcode-cn.com/"
+            },
+            {
+                "name": "Lintcode",
+                "url": "https://www.lintcode.com/problem"
+            },
+        ]
+    },
+    {
+        "c": "导图文案",
+        "a": [
+            {
+                "name": "Emoji大全",
+                "url": "https://emoji.svend.cc/"
+            },
+            {
+                "name": "字体下载",
+                "url": "http://www.fonts.net.cn"
+            },
+            {
+                "name": "PPT模板",
+                "url": "http://www.1ppt.com"
+            },
+            {
+                "name": "谐音工具",
+                "url": "http://www.wenangou.com/xieyin.html"
+            },
+            {
+                "name": "句子文案",
+                "url": "https://www.juzikong.com/"
+            },
+            {
+                "name": "流程图",
+                "url": "https://www.liuchengtu.com/"
+            },
+            {
+                "name": "百度脑图",
+                "url": "https://naotu.baidu.com/home"
+            },
+            {
+                "name": "GitMind",
+                "url": "https://gitmind.cn/"
+            },
+            {
+                "name": "文档翻译",
+                "url": "https://www.onlinedoctranslator.com/"
+            },
+            {
+                "name": "ProcessOn",
+                "url": "https://www.processon.com/"
+            },
+            {
+                "name": "Ascii流程图",
+                "url": "http://asciiflow.com/"
+            },
+        ]
+    },
+    {
+        "c": "数据文献",
+        "a": [
+            {
+                "name": "国家图书馆",
+                "url": "http://www.nlc.cn/"
+            },
+            {
+                "name": "国家科学院文献",
+                "url": "http://kgview.las.ac.cn/discover"
+            },
+            {
+                "name": "国家哲社科文献",
+                "url": "http://www.ncpssd.org/"
+            },
+            {
+                "name": "临时邮箱",
+                "url": "http://24mail.chacuo.net/"
+            },
+            {
+                "name": "全历史",
+                "url": "https://www.allhistory.com/"
+            },
+            {
+                "name": "金十数据",
+                "url": "https://www.jin10.com/"
+            },
+            {
+                "name": "增长黑客",
+                "url": "https://blog.growingio.com/"
+            },
+            {
+                "name": "数据报告",
+                "url": "https://www.fxbaogao.com/"
+            },
+            {
+                "name": "AI简历",
+                "url": "https://datayi.cn"
+            },
+        ]
+    },
+    {
+        "c": "开源标准",
+        "a": [
+            {
+                "name": "国家标准公开",
+                "url": "http://openstd.samr.gov.cn/"
+            },
+            {
+                "name": "国家标准信息平台",
+                "url": "http://std.samr.gov.cn/"
+            },
+            {
+                "name": "CAS",
+                "url": "http://www.china-cas.org/"
+            },
+            {
+                "name": "ODCC",
+                "url": "http://www.opendatacenter.cn/"
+            },
+            {
+                "name": "ISO",
+                "url": "https://www.iso.org/"
+            },
+            {
+                "name": "IEC",
+                "url": "https://www.iec.ch/"
+            },
+            {
+                "name": "ANSI",
+                "url": "https://www.ansi.org/"
+            },
+            {
+                "name": "ASME",
+                "url": "https://www.asme.org/"
+            },
+            {
+                "name": "BSI",
+                "url": "https://www.bsi.org/"
+            },
+            {
+                "name": "DIN",
+                "url": "https://www.din.de"
+            },
+            {
+                "name": "JISC",
+                "url": "https://www.jisc.go.jp/"
+            },
+            {
+                "name": "AFNOR",
+                "url": "https://www.afnor.org/"
+            },
+            {
+                "name": "FAQS",
+                "url": "http://www.faqs.org/"
+            },
+            {
+                "name": "APACHE",
+                "url": "https://www.apache.org/"
+            },
+            {
+                "name": "CNCF",
+                "url": "https://www.cncf.io/"
+            },
+            {
+                "name": "LinuxFoundation",
+                "url": "https://www.linuxfoundation.org/"
+            },
+        ]
+    },
+    {
+        "c": "图片工具",
+        "a": [
+            {
+                "name": "图片压缩转换",
+                "url": "https://zhitu.isux.us/"
+            },
+            {
+                "name": "图片压缩",
+                "url": "https://tinypng.com/"
+            },
+            {
+                "name": "在线抠图",
+                "url": "https://www.gaoding.com/koutu"
+            },
+            {
+                "name": "在线设计图片",
+                "url": "https://chuangkit.com"
+            },
+            {
+                "name": "图标库",
+                "url": "http://fontstore.baidu.com/store#!/"
+            },
+            {
+                "name": "矢量图标库",
+                "url": "https://www.iconfont.cn/"
+            },
+            {
+                "name": "代码格式图片",
+                "url": "https://carbon.now.sh/"
+            },
+        ]
+    },
+    {
+        "c": "速查手册",
+        "a": [
+            {
+                "name": "HTTP状态码",
+                "url": "https://tool.oschina.net/commons?type=5"
+            },
+            {
+                "name": "Linux命令",
+                "url": "http://linux.51yip.com/"
+            },
+            {
+                "name": "Git使用图解",
+                "url": "https://learngitbranching.js.org/?locale=zh_CN"
+            },
+            {
+                "name": "开发手册大全",
+                "url": "https://cloud.tencent.com/developer/devdocs"
+            },
+            ////{"name":"","url":""},
+        ]
+    },
+];
